ad1799bb52683a2962b235796bfc094f3c0ecf92
xsbchen <xsbchen@qq.com>
2021-09-07 12:15:12 +0800

    update logic

--- child.js
+++ child.js
@@ -3,9 +3,9 @@ const startAt = new Date();
 console.log('child %d started:', process.pid, startAt);
 
 while (new Date() - startAt <= 30 * 1000) {
-  if (new Date() - startAt > 5 * 1000 && Math.random() > 0.9) {
-    throw new Error('crashed');
-  }
+//   if (new Date() - startAt > 5 * 1000 && Math.random() > 0.9) {
+    // throw new Error('crashed');
+//   }
 }
 
 console.log('child %d exited:', process.pid, new Date() - startAt);
